88ec47712efc17595284fd637917d396536ed05d
Edwin Vanegas <edwinvasa@gmail.com>
2021-10-13 13:33:15 -0500

    comenzando el proyecto

---
+++ src/main/java/com/arquitecturahexagonal/ArquitecturahexagonalApplication.java
@@ -0,0 +1,13 @@
+package com.arquitecturahexagonal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArquitecturahexagonalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArquitecturahexagonalApplication.class, args);
+    }
+
+}
